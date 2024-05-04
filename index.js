b495319f8a394ab88b2441a922dced36c25792ea
Razzi Abuissa <razzi@abuissa.net>
2024-05-03 23:41:28 -0500

    File upload with express and multer

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs = require('node:fs');
+const path = require('path');
+const express = require('express');
+const mime = require('mime')
+const multer  = require('multer')
+
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: 'images',
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
+
+app.get('/', (req, res) => {
+  const options = {
+    root: path.join(__dirname)
+  }
+
+  res.sendFile('client.html', options)
+})
+
+app.post('/', upload.single('datafile'), (req, res) => {
+  res.end('Uploaded file')
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000.');
+});
